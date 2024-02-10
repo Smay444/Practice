71d3aff4f131894b9e665842eb3042abeba9dce1
smay444 <smay444>
2024-02-10 09:55:49 -0500

    Created nbYears function to calculate years to reach a certain population

--- Main.java
+++ Main.java
@@ -5,6 +5,15 @@ public class Main {
         int[] statues = {0,3, 6, 8};
         Practice.solution(statues);
 
+        int[] left = {5,10,15,20};
+        int[] right = {1,3,5,7,9};
+        Practice.merge(left,right);
+
+        int p0 = 1500;
+        double percentage = 5;
+        int aug = 100;
+        int p = 5000;
+        Practice.nbYear(p0, percentage, aug, p);
 
     }
 }
--- Practice.java
+++ Practice.java
@@ -18,7 +18,59 @@ public class Practice {
     }
 
 
-    //use a nested loop
-    //one loops starts at zero / one starts at one
+    //Example of a Merge Sort
+    int[] left = {5,10,15,20};
+    int[] right = {1,3,6,7,9};
+    public static int[] merge(int[] left, int[] right){
+
+        int totalLength = left.length + right.length;
+        int[] sortedArray = new int[totalLength];
+        int i = 0, j=0, k=0; //keeps track of left, right and sortedArrays
+
+        while(i < left.length && j < right.length){
+            if (left[i] < right[j]){
+                sortedArray[k] = left[i];
+                i++;
+            } else {
+                sortedArray[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < left.length){
+            sortedArray[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length){
+            sortedArray[k] = right[j];
+            j++;
+            k++;
+        }
+        System.out.println(Arrays.toString(sortedArray));
+
+
+        return sortedArray;
+    }
+
+    public static int nbYear (int p0, double percentage, int aug, int p){
+
+        int numOfYears = 0;
+        double currentPopulation = p0;
+        double percent = percentage / 100;
+
+        for (int i=0; i<p; i++) {
+            if (currentPopulation < p){
+                currentPopulation += currentPopulation * percent + aug;
+                numOfYears++;
+            } else {
+                System.out.println("The numbers of years required would be: " + numOfYears);
+                return numOfYears;
+            }
+
+
+        }
+        return numOfYears;
+    }
 
 }
